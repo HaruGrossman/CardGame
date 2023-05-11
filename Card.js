bcee8f48f9386f948720f761c3ccd733a6af0b14
HaruGrossman <dogtaboodog@yahoo.com>
2023-05-11 16:24:42 -0500

    end of thursday save

---
+++ Card.js
@@ -0,0 +1,16 @@
+// const suit = ["spades", "diamonds", "clubs", "hearts"];
+// const rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+// class Card {
+//     constructor(rank, suit) {
+//         this.rank = rank; // 2-10, jack, queen, king, ace
+//         this.suit = suit; // heart, diamond, spade, club
+//     }
+
+//     render() {
+//         const card = document.createElement("div");
+//         card.contentText = `${this.rank}${this.suit}`;
+//         return card;
+//     }
+// }
+
